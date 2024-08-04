6cf4f6ab729537878ee14ac468a00489cb429010
zack6yuan <zack6yuan@gmail.com>
2024-08-04 00:49:19 +0000

    completed the nop task

--- nop.c
+++ nop.c
@@ -1,1 +1,12 @@
 #include "monty.h"
+/**
+ * nop - does nothing
+ * @stack: the stack, nothing will be done to it
+ * @line_number: the number of the current line in the file
+ * Return: void
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+        (void)stack;
+        (void)line_number;
+}
