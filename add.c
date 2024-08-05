89f335f7d1cf256ad1309d9fd29b31cc03277371
zack6yuan <zack6yuan@gmail.com>
2024-08-05 19:01:15 +0000

    started add function

--- add.c
+++ add.c
@@ -1,4 +1,22 @@
 #include "monty.h"
 /**
  * add - adds the top two elements of the stack.
-*/
+ * @stack: the stack where elements are being added to
+ * @line_number: the current lline number of the file
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+        stack_t *node1;
+        stack_t *node2;
+
+        if (!*stack || !(*stack)->next)
+        {
+                fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        node1 = *stack;
+        node2 = (*stack)->next;
+
+        node2->n += node1->n;
+}
