4a5905c6fd849a28b7a0c30d8c6a02ee1d3159a0
zack6yuan <zack6yuan@gmail.com>
2024-08-05 14:53:26 +0000

    placed parenthesis in wrong place

--- push.c
+++ push.c
@@ -9,7 +9,7 @@ void push(stack_t **stack, unsigned int line_number, int n)
 {
         stack_t *node = malloc(sizeof(stack_t));
 
-        void(line_number); /*void because unused*/
+        (void)line_number; /*void because unused*/
 
         if (!node) /*checks if malloc failed*/
         {
