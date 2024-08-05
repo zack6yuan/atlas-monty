27fa11a0d7a6a5c6cf47f9d7fc03dd941c835211
zack6yuan <zack6yuan@gmail.com>
2024-08-05 14:30:47 +0000

    added function for pall.c

--- pall.c
+++ pall.c
@@ -1,1 +1,19 @@
 #include "monty.h"
+/**
+ * pall - prints all the elements of a stack
+ * @stack: the stack being pushed to
+ * @line_number: the current line in the file
+ * Return: void
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+        stack_t *node
+
+        (void)line_number;
+
+        while (node != NULL)
+        {
+                printf("&d\n", node->n); /*print val of node, newline*/
+                node = node->next; /*move to the next node*/
+        }
+}
