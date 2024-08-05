eb087f435c6401802caa8e760af760c6eb3bdf9d
zack6yuan <zack6yuan@gmail.com>
2024-08-05 19:04:11 +0000

    pop function

--- pop.c
+++ pop.c
@@ -1,20 +1,20 @@
 #include "monty.h"
 /**
  * pop - removes the top element of the stack
- * 
- * 
+ * @stack: the stack where the element is beign removed
+ * @line_number: the current line of the file
  * Return: void
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-        stack_t *node
+        stack_t *node;
 
         if (!*stack) /*checks if the stack is empty*/
         {
                 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
                 exit(EXIT_FAILURE);
         }
-        node = *stack /*top element of stack to node ptr*/
+        node = *stack; /*top element of stack to node ptr*/
         *stack = (*stack)->next; /*update to next*/
 
         if (*stack) /*checks if stack is empty*/
