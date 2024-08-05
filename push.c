b2a3969019045743e4839c25b5d107515bd56702
zack6yuan <zack6yuan@gmail.com>
2024-08-05 14:43:10 +0000

    completed push.c function

--- push.c
+++ push.c
@@ -5,7 +5,23 @@
  * @line_number: the current line in the file
  * Return: void
 */
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number, int n)
 {
+        stack_t *node = *stack;
+        node = malloc(sizeof(stack_t));
 
+        if (!new) /*checks if malloc failed*/
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                exit(EXIT_FAILURE);
+        }
+
+        node->prev = NULL; /*set prev to NULL*/
+        node->n = n; /*set valuie of new node to n */
+        node->next = *stack; /*set to the head*/
+        if (*stack)
+        {
+                (*stack)->prev = node;
+        }
+        *stack = node;
 }
