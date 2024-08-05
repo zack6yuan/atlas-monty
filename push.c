04a6979a213fd43f90ca3153c2cab3c9f4997109
zack6yuan <zack6yuan@gmail.com>
2024-08-05 14:30:56 +0000

    started push.c

--- push.c
+++ push.c
@@ -1,1 +1,11 @@
 #include "monty.h"
+/**
+ * push - pushes an element to the stack
+ * @stack: the stack being pushed to
+ * @line_number: the current line in the file
+ * Return: void
+*/
+void push(stack_t **stack, unsigned int line_number)
+{
+
+}
