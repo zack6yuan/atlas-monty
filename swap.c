c709b4e09297b1b7189e10f1c58d8e2956800876
zack6yuan <zack6yuan@gmail.com>
2024-08-05 18:52:48 +0000

    completed swap function

--- swap.c
+++ swap.c
@@ -1,11 +1,18 @@
 #include "monty.h"
 /**
  * swap - swaps the top two elements of a stack
- * 
- * 
+ * @stack: stack where the elements are being swapped
+ * @line_number: current line in file
  * Return: void
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
+        stack_t *node1;
+        stack_t *node2;
 
+        if (!(*stack) || !(*stack)->next)
+        {
+                fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
 }
