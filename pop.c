7df9c89a97a1e90217aa7e51aa4082f7af90dd4a
zack6yuan <zack6yuan@gmail.com>
2024-08-05 18:31:47 +0000

    finished pop function

--- pop.c
+++ pop.c
@@ -7,5 +7,19 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-        
+        stack_t *node
+
+        if (!*stack) /*checks if the stack is empty*/
+        {
+                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        node = *stack /*top element of stack to node ptr*/
+        *stack = (*stack)->next; /*update to next*/
+
+        if (*stack) /*checks if stack is empty*/
+        {
+                (*stack)->prev = NULL; /*set prev to NULL*/
+        }
+        free(node); /*free the memory allocated*/
 }
