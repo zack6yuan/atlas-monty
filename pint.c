5be67466f07abdcc5c7d0dba51dfb34193799ba6
zack6yuan <zack6yuan@gmail.com>
2024-08-05 15:01:57 +0000

    completed pint.c function

--- pint.c
+++ pint.c
@@ -1,1 +1,15 @@
 #include "monty.h"
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: points to the head of the stack
+ * @line_number: the current line number in the file
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+        if (!*stack)
+        {
+                fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+                exit(EXIT_FAILURE);
+        }
+}
