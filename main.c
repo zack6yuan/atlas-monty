8e3fd7bc88bc5288629155a625f6c70b39fbb5bb
zack6yuan <zack6yuan@gmail.com>
2024-08-06 02:23:46 +0000

    main file complete

--- main.c
+++ main.c
@@ -7,18 +7,35 @@
 */
 int main(int argc, char *argv[])
 {
-        FILE *file;
-        
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-        file = fopen(argv[1], "r");
-        if (!file)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+        FILE *file; /*pointer to the file*/
+        unsigned int line_number = 0; /*current line number*/
+        char *arg = NULL; /*stores opcode args*/
+        char *line_len = 0; /*stores lines read*/
+        stack_t *stack = NULL; /*ptr to head of stack*/
 
+        if (argc != 2) /*checks if number or arguments != 2*/
+        {
+                fprintf(stderr, "USAGE: monty file\n"); /*error message*/
+                exit(EXIT_FAILURE); /*exit*/
+        }
+        file = fopen(argv[1], "r"); /*open for reading*/
+        if (!file) /*if the file can't be opened*/
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]); /*error message*/
+                exit(EXIT_FAILURE); /*exit*/
+        }
+        while ((read = getline(&line, &len, file)) != -1) /*reads line using getline*/
+        {
+                line_number++; /*increment*/
+                opcode = strtok(line, " \t\n"); /*use strtok to tokenize*/
+                if (!opcode || opcode[0] == '#') /*if !opcode or comment*/
+                {
+                        continue;
+                }
+                exec_instruct(opcode, &stack, line_number); /*call exec_intrstruct to execute*/
+        }
+        free(line); /*free memory allocated for line*/
+        fclose(file); /*close the file*/
+        free_stack(stack); /*free the stack*/
+        return (EXIT_SUCCESS); /*exit with success!*/
 }
